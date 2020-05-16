59791b218033e43a297d2aa0270c062c9429aaf5
cow-co <cow-co@users.noreply.github.com>
2020-05-16 16:46:54 +0100

    Add Badges to Readme

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./config/keys").mongo_uri;
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB connection successful"))
+  .catch((err) => console.error(err));
