6ca10e29811f2e29ad35e79cb42837eb9a223dd0
cow-co <cow-co@users.noreply.github.com>
2020-05-17 14:58:26 +0100

    Implement User Creation Endpoint

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const statusCodes = require("../../config/status_codes");
-const { getUserByName, getUserByEmail } = require("../../db_interface/users");
+const userMethods = require("../../db_interface/users");
 
 router.post("/login", async (req, res) => {
 	const loginDetails = req.body;
@@ -10,11 +10,11 @@ router.post("/login", async (req, res) => {
 	let response = null;
 
 	try {
-		const user = await getUserByName(loginDetails.name);
+		const user = await userMethods.getUserByName(loginDetails.name);
 		if(user) {
-			valid = await user.checkPassword(loginDetails.password);
+			valid = await userMethods.checkPassword(user, loginDetails.password);
 			if(valid) {
-				const jwt = await user.generateJWT();
+				const jwt = await userMethods.generateJWT(user);
 				response = res.json(jwt);
 			} else {
 				console.log("Invalid PW");
@@ -24,6 +24,35 @@ router.post("/login", async (req, res) => {
 		} else {
 			console.log("Invalid user");
 			response = res.status(statusCodes.CREDS_ERROR).json({ errors: ["Incorrect Credentials"] });
+		}
+	} catch(error) {
+		console.error("Other error");
+		response = res.status(statusCodes.SERVER_ERROR).json({ errors: ["Server Error"] });
+	}
+
+	return response;
+});
+
+router.post("/create", async (req, res) => {
+	const userDetails = req.body;
+	console.log("Received user-creation request");
+	let response = null;
+	let userExists = false;
+
+	try {
+		let user = await userMethods.getUserByName(userDetails.name);
+		if(user) {
+			userExists = true;
+		} else {
+			user = await userMethods.getUserByEmail(userDetails.email);
+			userExists = user ? true : false;
+		}
+
+		if(userExists) {
+			response = res.status(statusCodes.INVALID_STATUS).json({errors: ["User already exists!"]});
+		} else {
+			const userId = await userMethods.createUser(userDetails);
+			response = res.json({"id": userId});
 		}
 	} catch(error) {
 		console.error("Other error");
