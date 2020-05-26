8e5c9730cdf09745ad8e0edb1101d865f236caff
cow-co <cow-co@users.noreply.github.com>
2020-05-26 19:14:58 +0100

    Add More Logging to Email-Sending

--- helpers/emailing.js
+++ helpers/emailing.js
@@ -4,14 +4,18 @@ const SENDGRID_API_KEY = require("../config/keys").SENDGRID_API_KEY;
 sgMail.setApiKey(SENDGRID_API_KEY);
 
 const sendVerificationEmail = async (username, email, verificationToken) => {
-	const msg = {
-		to: email,
-		from: "verification@flight-sim-logbook.com",
-		subject: "Flight Sim Logbook Email Verification",
-		text: `Please click the link, or paste the link into your browser, to verify your email.\n\n`,
-		html: `Please click the button, or paste the link into your browser, to verify your email.<br/><br/><a href = "">Verify!</a>`
-	};
-	await sgMail.send(msg);
+	try {
+		const msg = {
+			to: email,
+			from: "verification@flight-sim-logbook.com",
+			subject: "Flight Sim Logbook Email Verification",
+			text: `Please click the link, or paste the link into your browser, to verify your email.\n\n`,
+			html: `Please click the button, or paste the link into your browser, to verify your email.<br/><br/><a href = "">Verify!</a>`
+		};
+		await sgMail.send(msg);
+	} catch(error) {
+		console.error(error);
+	}	
 }
 
 module.exports = { sendVerificationEmail }
