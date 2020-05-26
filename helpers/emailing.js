07eef8ecd078971598ddec687411fb1036e5e73b
cow-co <cow-co@users.noreply.github.com>
2020-05-26 20:54:30 +0100

    Add URL to Email

--- helpers/emailing.js
+++ helpers/emailing.js
@@ -18,8 +18,8 @@ const sendVerificationEmail = async (username, email, url) => {
 		to: email,
 		from: "verification@flight-sim-logbook.herokuapp.com",
 		subject: "Flight Sim Logbook Email Verification",
-		text: `Hello ${username}!\n\nPlease click the link, or paste the link into your browser, to verify your email.\n\n`,
-		html: `Hello ${username}!<br/><br/>Please click the link, or paste the link into your browser, to verify your email.<br/><br/><a href = "">Verify!</a>`
+		text: `Hello ${username}!\n\nPlease click the link, or paste the link into your browser, to verify your email.\n\n${url}`,
+		html: `Hello ${username}!<br/><br/>Please click the link, or paste the link into your browser, to verify your email.<br/><br/><a href = "${url}">Verify!</a>`
 	};
 	transport.sendMail(msg, (err, info) => {
 		if(err) {
