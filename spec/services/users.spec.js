804cef8ea44479227cb69ddee45035be12e26352
cow-co <cow-co@users.noreply.github.com>
2020-05-30 17:15:50 +0100

    Add More User-Creation Tests

--- spec/services/users.spec.js
+++ spec/services/users.spec.js
@@ -24,4 +24,68 @@ describe("User service tests", () => {
       fail();
     }
   });
+
+  it("Should fail to create a user with an empty email", async () => {
+    const invalidUser = {
+      name: "name",
+      email: "",
+      password: "password12345",
+      passwordConfirmation: "password12345",
+    };
+
+    try {
+      const user = await userService.createUser(invalidUser);
+      expect(user.errors.length).to.equal(1);
+    } catch (err) {
+      fail();
+    }
+  });
+
+  it("Should fail to create a user with an empty password", async () => {
+    const invalidUser = {
+      name: "name",
+      email: "someone@something.com",
+      password: "",
+      passwordConfirmation: "",
+    };
+
+    try {
+      const user = await userService.createUser(invalidUser);
+      expect(user.errors.length).to.equal(1);
+    } catch (err) {
+      fail();
+    }
+  });
+
+  it("Should fail to create a user with a non-matching password", async () => {
+    const invalidUser = {
+      name: "name",
+      email: "someone@something.com",
+      password: "password12345",
+      passwordConfirmation: "password12346",
+    };
+
+    try {
+      const user = await userService.createUser(invalidUser);
+      expect(user.errors.length).to.equal(1);
+    } catch (err) {
+      fail();
+    }
+  });
+
+  it("Should create a user", async () => {
+    const invalidUser = {
+      name: "name",
+      email: "someone@something.com",
+      password: "password12345",
+      passwordConfirmation: "password12345",
+    };
+
+    try {
+      const user = await userService.createUser(invalidUser);
+      expect(user.name).to.equal("name");
+    } catch (err) {
+      fail();
+    }
+  });
 });
