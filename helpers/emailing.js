6c3a64e64ca54722c5f66a3e3c4e64c138c542cf
cow-co <cow-co@users.noreply.github.com>
2020-07-09 10:25:14 +0100

    Return to Using Nodemailer
    Because Sendgrid is bad.

--- helpers/emailing.js
+++ helpers/emailing.js
@@ -1,31 +1,58 @@
-const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const mailer = require("nodemailer");
+const EMAIL_PW = require("../config/keys").EMAIL_PW;
+const EMAIL_UN = require("../config/keys").EMAIL_UN;
+let transport = null;
 
-const sendVerificationEmail = async (username, email, url) => {
+// Don't want to send emails in a dev environment
+if (process.env.NODE_ENV === "production") {
+  console.log("Creating transport");
+  transport = mailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: EMAIL_UN,
+      pass: EMAIL_PW,
+    },
+  });
+  if (!transport) {
+    console.log("Transport creation failed");
+  } else {
+    console.log(transport);
+  }
+}
+
+const sendVerificationEmail = (username, email, url) => {
   const msg = {
     to: email,
-    from: "verification@flight-sim-logbook.herokuapp.com",
+    from: "noreply@flight-sim-logbook.herokuapp.com",
     subject: "Flight Sim Logbook Email Verification",
     text: `Hello ${username}!\n\nPlease click the link, or paste the link into your browser, to verify your email.\n\n${url}`,
     html: `Hello ${username}!<br/><br/>Please click the link, or paste the link into your browser, to verify your email.<br/><br/><a href = "${url}">Verify!</a>`,
   };
   // Don't want to send emails in a dev environment
   if (process.env.NODE_ENV === "production") {
-    await sgMail.send(msg);
+    transport.sendMail(msg, (err, info) => {
+      if (err) {
+        console.log(err);
+      }
+    });
   }
 };
 
-const sendResetEmail = async (username, email, url) => {
+const sendResetEmail = (username, email, url) => {
   const msg = {
     to: email,
-    from: "reset@flight-sim-logbook.herokuapp.com",
+    from: "noreply@flight-sim-logbook.herokuapp.com",
     subject: "Flight Sim Logbook Password Reset",
     text: `Hello ${username}!\n\nPlease click the link, or paste the link into your browser, to reset your password.\n\n${url}`,
     html: `Hello ${username}!<br/><br/>Please click the link, or paste the link into your browser, to reset your password.<br/><br/><a href = "${url}">Reset Password!</a>`,
   };
   // Don't want to send emails in a dev environment
   if (process.env.NODE_ENV === "production") {
-    await sgMail.send(msg);
+    transport.sendMail(msg, (err, info) => {
+      if (err) {
+        console.log(err);
+      }
+    });
   }
 };
 
