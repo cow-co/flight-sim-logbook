c82dd0e6c6700a53e69151d8d8c2097cba2478d5
cow-co <cow-co@users.noreply.github.com>
2020-07-18 11:33:27 +0100

    Use React Router Redirect

--- client/src/pages/users/Login.jsx
+++ client/src/pages/users/Login.jsx
@@ -3,6 +3,7 @@ import { TextField, Button, Grid } from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
 import { login } from "../../redux/actions/user-actions";
 import "./Login.css";
+import { Redirect } from "react-router-dom";
 
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
@@ -29,55 +30,55 @@ class Login extends React.Component {
   sendLogin = async (event) => {
     event.preventDefault();
     await this.props.login(this.state);
-    if (this.props.users.isLoggedIn) {
-      console.log("pushing");
-      window.history.push("/");
-    }
   };
 
   render() {
-    return (
-      <div className="login">
-        <Typography variant="h4" className="title">
-          Login
-        </Typography>
-        <form onSubmit={this.sendLogin}>
-          <Grid
-            className="group-list-grid"
-            container
-            spacing={3}
-            direction="column"
-            alignItems="center"
-            justify="center"
-          >
-            <Grid item>
-              <TextField
-                id="username"
-                label="Username"
-                value={this.state.username}
-                onChange={this.updateField}
-                required
-              />
+    if (this.props.users.isLoggedIn) {
+      return <Redirect to="/" />;
+    } else {
+      return (
+        <div className="login">
+          <Typography variant="h4" className="title">
+            Login
+          </Typography>
+          <form onSubmit={this.sendLogin}>
+            <Grid
+              className="group-list-grid"
+              container
+              spacing={3}
+              direction="column"
+              alignItems="center"
+              justify="center"
+            >
+              <Grid item>
+                <TextField
+                  id="username"
+                  label="Username"
+                  value={this.state.username}
+                  onChange={this.updateField}
+                  required
+                />
+              </Grid>
+              <Grid item>
+                <TextField
+                  type="password"
+                  id="password"
+                  label="Password"
+                  value={this.state.password}
+                  onChange={this.updateField}
+                  required
+                />
+              </Grid>
+              <Grid item>
+                <Button variant="contained" size="large" color="primary" type="submit">
+                  Log In
+                </Button>
+              </Grid>
             </Grid>
-            <Grid item>
-              <TextField
-                type="password"
-                id="password"
-                label="Password"
-                value={this.state.password}
-                onChange={this.updateField}
-                required
-              />
-            </Grid>
-            <Grid item>
-              <Button variant="contained" size="large" color="primary" type="submit">
-                Log In
-              </Button>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-    );
+          </form>
+        </div>
+      );
+    }
   }
 }
 
