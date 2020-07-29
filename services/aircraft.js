b824e78ed602cf65f11e4a431870b183b2c685bc
cow-co <cow-co@users.noreply.github.com>
2020-07-29 21:23:25 +0100

    Remove Debug Message

--- services/aircraft.js
+++ services/aircraft.js
@@ -1,7 +1,6 @@
 const Aircraft = require("../models/Aircraft");
 
 const findAircraftByName = async (aircraftName) => {
-  console.log(aircraftName);
   let aircraft = null;
   aircraft = await Aircraft.findOne({ name: aircraftName });
   return aircraft;
