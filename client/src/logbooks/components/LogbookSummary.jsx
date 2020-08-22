32ee1dcc942c0b38cb978653f1e198f9c72bc9ba
cow-co <cow-co@users.noreply.github.com>
2020-08-22 21:47:02 +0100

    Use a Button for Viewing a Logbook

--- client/src/logbooks/components/LogbookSummary.jsx
+++ client/src/logbooks/components/LogbookSummary.jsx
@@ -3,6 +3,7 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import { Link } from "react-router-dom";
 import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import PageViewIcon from "@material-ui/icons/PageView";
 import Grid from "@material-ui/core/Grid";
 import { Button } from "@material-ui/core";
 import "./LogbookSummary.css";
@@ -15,26 +16,34 @@ class LogbookSummary extends React.Component {
   render() {
     return (
       <ListItem button>
-        <Link to={`/logbooks/${this.props.username}/${this.props.logbook.aircraftName}`}>
-          <Grid container spacing={3}>
-            <Grid item xs={6}>
-              <ListItemText
-                primary={`${this.props.logbook.aircraft}`}
-                secondary={`Hours: ${this.props.logbook.totalHours}; Kills: ${this.props.logbook.a2aKills}`}
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <Button
-                color="primary"
-                variant="contained"
-                startIcon={<DeleteForeverIcon />}
-                onClick={this.sendDeleteRequest}
-              >
-                Create
-              </Button>
-            </Grid>
+        <Grid container spacing={3}>
+          <Grid item xs={4}>
+            <ListItemText
+              primary={`${this.props.logbook.aircraft}`}
+              secondary={`Hours: ${this.props.logbook.totalHours}; Kills: ${this.props.logbook.a2aKills}`}
+            />
           </Grid>
-        </Link>
+          <Grid item xs={4}>
+            <Button
+              color="primary"
+              variant="contained"
+              startIcon={<PageViewIcon />}
+              to={`/logbooks/${this.props.username}/${this.props.logbook.aircraft}`}
+            >
+              Create
+            </Button>
+          </Grid>
+          <Grid item xs={4}>
+            <Button
+              color="secondary"
+              variant="contained"
+              startIcon={<DeleteForeverIcon />}
+              onClick={this.sendDeleteRequest}
+            >
+              Create
+            </Button>
+          </Grid>
+        </Grid>
       </ListItem>
     );
   }
