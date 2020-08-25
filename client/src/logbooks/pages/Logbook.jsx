2245b52447c0d68456c933af88830d719bc0fe9f
cow-co <cow-co@users.noreply.github.com>
2020-08-25 23:21:23 +0100

    Add Radar Chart

--- client/src/logbooks/pages/Logbook.jsx
+++ client/src/logbooks/pages/Logbook.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
 import RadarChart from "react-svg-radar-chart";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
 import "react-svg-radar-chart/build/css/index.css";
 
 class Logbook extends React.Component {
@@ -12,11 +13,24 @@ class Logbook extends React.Component {
   // TODO Radar chart for main logbook factors
   // TODO Button to add a mission
   render() {
+    let radarData = this.props.logbook;
+    delete radarData.totalHours;
+    delete radarData.a2aKills;
+
     return (
       <div>
         <Typography variant="h4" className="title">
           {this.props.logbook.aircraft}
         </Typography>
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            <AccessTimeIcon />
+            <Typography variant="h6">{this.props.logbook.totalHours} Hours</Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <RadarChart data={radarData} size={400} />
+          </Grid>
+        </Grid>
       </div>
     );
   }
